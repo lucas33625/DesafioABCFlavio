f54e5b2406de154681ed68768948908015d11de8
Lucas Ramos <lucas.ramos@abctechnology.com.br>
2025-04-22 20:24:34 -0300

    feat(db): cria tabela cliente_auditoria para registrar alterações de dados dos clientes

---
+++ desafio/src/main/resources/db/migration/V__2_inserir_auditoria.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cliente_auditoria (
+                                   id INT AUTO_INCREMENT PRIMARY KEY,
+                                   cliente_id INT NOT NULL,
+                                   campo_alterado VARCHAR(255) NOT NULL,
+                                   valor_antigo VARCHAR(255),
+                                   valor_novo VARCHAR(255),
+                                   data_alteracao DATETIME NOT NULL
+);
